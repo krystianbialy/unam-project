c84d9a10671d7b1e73e54bfbd147c3ed8c4d3599
krystianbialy <krystian.bialy@onet.pl>
2020-02-04 23:46:40 +0100

    cors test 4

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-  header('Access-Control-Allow-Origin', 'http://localhost:8771'); 
+  header('Access-Control-Allow-Origin', 'https://unam-project.netlify.com'); 
   header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   return send(200);
