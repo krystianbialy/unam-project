c7475fb40dbaaa9a18f920adfaa8dd44710e7be2
krystianbialy <krystian.bialy@onet.pl>
2020-02-04 18:04:08 +0100

    Heroku fixes

--- webpack.prod.js
+++ webpack.prod.js
@@ -152,8 +152,7 @@ module.exports = {
         copy: [
           { source: './dist/img', destination: './dist/en/img' },
           { source: './src/php', destination: './dist' },
-          { source: './src/php/contact.php', destination: './dist/en' },
-          { source: './composer.json', destination: './dist' }
+          { source: './src/php', destination: './dist/en' }
         ]
       }
     })
