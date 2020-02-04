bc1520fd84a04f0f2ecc45149fa88e4ef9902daa
krystianbialy <krystian.bialy@onet.pl>
2020-02-05 00:54:38 +0100

    cors test 5

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
-  header('Access-Control-Allow-Origin', 'https://unam-project.netlify.com'); 
+  header('Access-Control-Allow-Origin', '*'); 
   header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  return send(200);
 
  // email address verification
   function isEmail($email) {
