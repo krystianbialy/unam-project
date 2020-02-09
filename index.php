c9ffa4cdcd25913e1d222cb01401bf619c53091f
krystianbialy <krystian.bialy@onet.pl>
2020-02-09 01:13:34 +0100

    SendGrid test 4

--- index.php
+++ index.php
@@ -76,7 +76,10 @@ if ($_POST)
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
         curl_close($ch);
+
+        echo $response;
     }
+    echo json_encode($array);
 }
 
 ?>
